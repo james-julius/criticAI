dfe24564cbbf4ed7fbc5e91fbfaa346517b7cd35
James <jamesjulius@Jamess-MB-Pro.local>
2020-02-16 22:09:30 +0000

    Refactored to use hooks

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,29 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Menu.scss';
 
-class Menu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hamburgerActive: false
-        }
-        this.handleClick = this.handleClick.bind(this);
+function Menu(props) {
+    const [burgerActive, setBurgerActive] = useState(false);
+
+    function handleClick() {
+        props.toggleMenu();
+        setBurgerActive(!burgerActive);
     }
     
-    handleClick() {
-        this.props.toggleMenu();
-        this.setState({hamburgerActive: !this.state.hamburgerActive})
-    }
-    
-    render() {return (
+    return (
         <div id="menu">
-            <button onClick ={this.handleClick} class={this.state.hamburgerActive ? 'hamburger hamburger--arrow is-active' : 'hamburger hamburger--arrow'} type="button">
+            <button onClick ={handleClick} class={burgerActive ? 'hamburger hamburger--arrow is-active' : 'hamburger hamburger--arrow'} type="button">
                 <span class="hamburger-box">
                     <span class="hamburger-inner"></span>
                 </span>
             </button>
         </div>
-    )}
+    )
 }
 
 export default Menu;

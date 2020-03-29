ff9f15a91528a4f186753174c067d01e96432191
James <jamesdarby7@gmail.com>
2020-03-29 11:06:13 +0100

    Places API added

---
+++ src/API/placesAPI.js
@@ -0,0 +1,9 @@
+const placesAPI = {
+    const key = 'AIzaSyAzrSNVy0S5lrHzJlJ2iCpcpTvdVK__0kc';
+    autoComplete(input) {
+        let baseUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/output?input=${}&key=${}`;
+    }
+
+}  
+
+export default placesAPI;
